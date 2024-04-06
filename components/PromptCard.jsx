ecea81ab5582d579b7914632d94147587057d377
Hirwa Arnold <macbook@macbooks-MacBook-Pro.local>
2024-04-06 23:56:17 +0200

    Post dashboard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,20 +5,45 @@ import { useSession } from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
 
 function PromptCard({ post, handleTagClick, handleEdig, handleDelete }) {
+  const [ copied, setCopied ] = useState("")
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(""), 3000)
+  }
   return (
-    <div>
-      <Image
-         src={post.creator.image}
-         alt="user_image"
-         width={40}
-         height={40}
-         className = "rounded-full object-contain"
-      />
-      <div className="flex flex-col">
-        <h3 className="font-satoshi font-semibold text-gray-900">
-          {post.creator.username}
-        </h3>
+    <div className="prompt_card">
+      <div className="flex justify-between items-center gap-3 cursor-pointer">
+        <div>
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className = "rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              { post.creator.username }
+            </h3>
+            <p className="font-enter text-sma text-gray-500">{ post.creator.email }</p>
+          </div>
+        </div>
+        <div className="copy-btn" onClick={() => { handleCopy() }}>
+          <Image 
+            src={copied === post.PromptCard
+              ? '/assets/icons/tick.svg'
+              : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
+        </div>
       </div>
+      <p className="my-4 font-satoshi text-sm txt-gray-700">{post.prompt}</p>
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick = {() => handleTagClick && handleTagClick(post.tag) }
+      >{post.tag}</p>
     </div>
   )
 }
